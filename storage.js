b693c890c2a2830a0fc843c456b2d338869c12a7
Rakhaev Viktor <v.rakhaev@rambler-co.ru>
2016-07-31 12:27:43 +0300

    Test app storage class

--- storage.js
+++ storage.js
@@ -1,42 +1,56 @@
 var TarantoolConnection = require('tarantool-driver');
-var conn = new TarantoolConnection({host:'127.0.0.1',port: 3301});
-var connected = conn.connect();
-
-const getColors = word => {
-  return connected
-    .then(() => conn.select('colors', 'pk', 1, 0, 'eq', [word]))
-    .then(result => {
-      if (result != "undefined" && result[0] != "undefined" && result[0][0] != "undefined") {
-        return result[0][1];
-      }
-    })
-    .catch(err => console.log(err));
-}
-
-const addColors = (words, colors) => {
-  return Promise.all(words.map(word => {
-    return getColors(word)
-      .then(existColors => {
-        existColors = existColors || [];
-        var newColors = existColors.concat(colors);
-        var colorsList = [];
-        newColors = newColors.filter(el => {
-          return colorsList.indexOf(el.join()) == -1;
-        });
-
-        conn
-          .delete('colors', 'pk', [word])
-          .then(() => conn.insert('colors', [word, newColors]))
-          .catch(err => console.error(err));
-      })
-  }));
-}
-
-const getWordsFilter = () => connected.then(() => conn.select('word_filters', 'pk', 1, 0, 'all', []));
-const addWordsFilter = (word) => connected.then(() => conn.insert('word_filters', [word]));
-
-module.exports = {
-  getColors,
-  addColors,
-  getWordsFilter,
+const DEFAULT_OPTIONS = {
+    host:'127.0.0.1',
+    port: 3301
 };
+
+class AppStorage {
+    constructor(options) {
+        this.conn = new TarantoolConnection(Object.assign({}, DEFAULT_OPTIONS, options));
+        this.connected = this.conn.connect();
+    }
+
+    getWordsFilter() {
+        return this.connected.then(() => this.conn.select('word_filters', 'pk', 1, 0, 'all', []))
+    }
+
+    addWordsFilter(word) {
+        return this.connected.then(() => this.conn.insert('word_filters', [word]))
+    }
+
+    getColors(word) {
+        return this.connected
+            .then(() => this.conn.select('colors', 'pk', 1, 0, 'eq', [word]))
+            .then(result => {
+                if (result != "undefined" && result[0] != "undefined" && result[0][0] != "undefined") {
+                    return result[0][1];
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+    addColor(words, colors) {
+        return Promise.all(words.map(word => {
+            return this.getColors(word)
+                .then(existColors => {
+                    existColors = existColors || [];
+                    var newColors = existColors.concat(colors);
+                    var colorsList = [];
+                    newColors = newColors.filter(el => {
+                        return colorsList.indexOf(el.join()) == -1;
+                    });
+
+                    this.conn
+                        .delete('colors', 'pk', [word])
+                        .then(() => conn.insert('colors', [word, newColors]))
+                        .catch(err => console.error(err));
+                })
+        }));
+    }
+}
+
+module.exports = AppStorage;
+
+var s = new AppStorage();
+
+s.getWordsFilter().then(res => console.log(res));
